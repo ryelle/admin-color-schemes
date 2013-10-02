d0eacd17a223cb7c7281e881582c0996666108df
Kelly Dwan <kelly.dwan@gmail.com>
2013-10-02 13:06:32 -0400

    Check if `mp6_add_admin_colors` exists, rename Navy to Midnight

--- mp6-color-scheme.php
+++ mp6-color-scheme.php
@@ -8,24 +8,26 @@
  * License: GPL2
  */
 function kd_mp6_add_colors() {
-	mp6_add_admin_colors( 'sunset', array(
-		'label' => 'Sunset',
-		'palette' => array( '#b43c38', '#cf4944', '#dd823b', '#ccaf0b' ),
-		'icon' => array( 'base' => '#f3f1f1', 'focus' => '#fff', 'current' => '#fff' ),
-		'admin_path' => plugin_dir_path( __FILE__ ) . "sunset/admin-colors.css",
-		'admin_url'  => plugins_url( "sunset/admin-colors.css", __FILE__ ),
-		'customizer_path' => plugin_dir_path( __FILE__ ) . "sunset/customizer.css",
-		'customizer_url'  => plugins_url( "sunset/customizer.css", __FILE__ ),
-	) );
-	
-	mp6_add_admin_colors( 'navy', array(
-		'label' => 'Navy',
-		'palette' => array( '#25282b', '#363b3f', '#69a8bb', '#e14d43' ),
-		'icon' => array( 'base' => '#f1f2f3', 'focus' => '#fff', 'current' => '#fff' ),
-		'admin_path' => plugin_dir_path( __FILE__ ) . "navy/admin-colors.css",
-		'admin_url'  => plugins_url( "navy/admin-colors.css", __FILE__ ),
-		'customizer_path' => plugin_dir_path( __FILE__ ) . "navy/customizer.css",
-		'customizer_url'  => plugins_url( "navy/customizer.css", __FILE__ ),
-	) );
+	if ( function_exists( 'mp6_add_admin_colors' ) ) {
+		mp6_add_admin_colors( 'sunset', array(
+			'label' => 'Sunset',
+			'palette' => array( '#b43c38', '#cf4944', '#dd823b', '#ccaf0b' ),
+			'icon' => array( 'base' => '#f3f1f1', 'focus' => '#fff', 'current' => '#fff' ),
+			'admin_path' => plugin_dir_path( __FILE__ ) . "sunset/admin-colors.css",
+			'admin_url'  => plugins_url( "sunset/admin-colors.css", __FILE__ ),
+			'customizer_path' => plugin_dir_path( __FILE__ ) . "sunset/customizer.css",
+			'customizer_url'  => plugins_url( "sunset/customizer.css", __FILE__ ),
+		) );
+		
+		mp6_add_admin_colors( 'navy', array(
+			'label' => 'Midnight',
+			'palette' => array( '#25282b', '#363b3f', '#69a8bb', '#e14d43' ),
+			'icon' => array( 'base' => '#f1f2f3', 'focus' => '#fff', 'current' => '#fff' ),
+			'admin_path' => plugin_dir_path( __FILE__ ) . "midnight/admin-colors.css",
+			'admin_url'  => plugins_url( "midnight/admin-colors.css", __FILE__ ),
+			'customizer_path' => plugin_dir_path( __FILE__ ) . "midnight/customizer.css",
+			'customizer_url'  => plugins_url( "midnight/customizer.css", __FILE__ ),
+		) );
+	}
 }
 add_action( 'admin_init' , 'kd_mp6_add_colors', 7 );
