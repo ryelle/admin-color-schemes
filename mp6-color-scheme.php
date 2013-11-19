16ab699f8cab51c127ddaf610af87a7e3f66120d
Kelly Dwan <kelly.dwan@gmail.com>
2013-11-18 21:12:48 -0500

    Updated to work with MP6 now in trunk
    Remove `mp6_add_admin_colors`, which was basically a rewrite/wrapper
    for `wp_admin_css_color`.
    Make sure colors-fresh is a dependency, since we can’t reliably import
    in plugin CSS.

--- mp6-color-scheme.php
+++ mp6-color-scheme.php
@@ -7,47 +7,58 @@
  * Author URI: http://redradar.net
  * License: GPL2
  */
-function kd_mp6_add_colors() {
-	if ( function_exists( 'mp6_add_admin_colors' ) ) {
-		mp6_add_admin_colors( 'sunset', array(
-			'label' => 'Sunset',
-			'palette' => array( '#b43c38', '#cf4944', '#dd823b', '#ccaf0b' ),
-			'icon' => array( 'base' => '#f3f1f1', 'focus' => '#fff', 'current' => '#fff' ),
-			'admin_path' => plugin_dir_path( __FILE__ ) . "sunset/admin-colors.css",
-			'admin_url'  => plugins_url( "sunset/admin-colors.css", __FILE__ ),
-		) );
+function admin_schemes_add_colors() {
+	wp_admin_css_color( 
+		'sunset', __( 'Sunset', 'admin_schemes' ), 
+		plugins_url( "sunset/admin-colors.css", __FILE__ ),
+		array( '#b43c38', '#cf4944', '#dd823b', '#ccaf0b' ), 
+		array( 'base' => '#f3f1f1', 'focus' => '#fff', 'current' => '#fff' )
+	);
 
-		mp6_add_admin_colors( 'vineyard', array(
-			'label' => 'Vineyard',
-			'palette' => array( '#301D25', '#462b36', '#d3995d', '#eabe3f' ),
-			'icon' => array( 'base' => '#f1f2f3', 'focus' => '#fff', 'current' => '#fff' ),
-			'admin_path' => plugin_dir_path( __FILE__ ) . "vineyard/admin-colors.css",
-			'admin_url'  => plugins_url( "vineyard/admin-colors.css", __FILE__ ),
-		) );
+	wp_admin_css_color( 
+		'vinyard', __( 'Vinyard', 'admin_schemes' ), 
+		plugins_url( "vineyard/admin-colors.css", __FILE__ ),
+		array( '#301D25', '#462b36', '#d3995d', '#eabe3f' ), 
+		array( 'base' => '#f1f2f3', 'focus' => '#fff', 'current' => '#fff' )
+	);
 
-		mp6_add_admin_colors( 'primary', array(
-			'label' => 'Primary',
-			'palette' => array( '#282b48', '#35395c', '#f38135', '#e7c03a' ),
-			'icon' => array( 'base' => '#f1f2f3', 'focus' => '#fff', 'current' => '#fff' ),
-			'admin_path' => plugin_dir_path( __FILE__ ) . "primary/admin-colors.css",
-			'admin_url'  => plugins_url( "primary/admin-colors.css", __FILE__ ),
-		) );
+	wp_admin_css_color( 
+		'primary', __( 'Primary', 'admin_schemes' ), 
+		plugins_url( "primary/admin-colors.css", __FILE__ ),
+		array( '#282b48', '#35395c', '#f38135', '#e7c03a' ),
+		array( 'base' => '#f1f2f3', 'focus' => '#fff', 'current' => '#fff' )
+	);
 
-		mp6_add_admin_colors( 'mint', array(
-			'label' => 'Mint',
-			'palette' => array( '#4f6d59', '#33834e', '#5FB37C', '#81c498' ),
-			'icon' => array( 'base' => '#f1f3f2', 'focus' => '#fff', 'current' => '#fff' ),
-			'admin_path' => plugin_dir_path( __FILE__ ) . "primary/admin-colors.css",
-			'admin_url'  => plugins_url( "mint/admin-colors.css", __FILE__ ),
-		) );
-		
-		mp6_add_admin_colors( 'evergreen', array(
-			'label' => 'Evergreen',
-			'palette' => array( '#324d3a', '#446950', '#56b274', '#324d3a' ),
-			'icon' => array( 'base' => '#f1f3f2', 'focus' => '#fff', 'current' => '#fff' ),
-			'admin_path' => plugin_dir_path( __FILE__ ) . "primary/admin-colors.css",
-			'admin_url'  => plugins_url( "evergreen/admin-colors.css", __FILE__ ),
-		) );
-	}
+	wp_admin_css_color( 
+		'mint', __( 'Mint', 'admin_schemes' ), 
+		plugins_url( "mint/admin-colors.css", __FILE__ ),
+		array( '#4f6d59', '#33834e', '#5FB37C', '#81c498' ),
+		array( 'base' => '#f1f3f2', 'focus' => '#fff', 'current' => '#fff' )
+	);
+
+	wp_admin_css_color( 
+		'evergreen', __( 'Evergreen', 'admin_schemes' ), 
+		plugins_url( "evergreen/admin-colors.css", __FILE__ ),
+		array( '#324d3a', '#446950', '#56b274', '#324d3a' ),
+		array( 'base' => '#f1f3f2', 'focus' => '#fff', 'current' => '#fff' )
+	);
 }
-add_action( 'admin_init' , 'kd_mp6_add_colors', 7 );
+add_action( 'admin_init' , 'admin_schemes_add_colors' );
+
+/**
+ * Make sure `colors-fresh.css` gets enqueued, since we can't @import it
+ * from a plugin stylesheet.
+ */ 
+function admin_schemes_load_mp6_default_css() {
+
+	global $wp_styles, $_wp_admin_css_colors;
+
+	$color_scheme = get_user_option( 'admin_color' );
+	
+	if ( $color_scheme == 'fresh' )
+		return;
+
+	$wp_styles->registered[ 'colors' ]->deps[] = 'colors-fresh';
+
+}
+add_action( 'admin_init', 'admin_schemes_load_mp6_default_css', 20 );
