e003e177afeff2a10f5322f79035ca3f75a5a63c
Eduardo Zulian <eduardozulian@gmail.com>
2014-01-04 01:39:44 -0200

    Add translation support
    Move all color schemes into a /colors folder, change the text domain to
    match plugin slug and add translation support as described in #3

--- admin-color-schemes.php
+++ admin-color-schemes.php
@@ -6,7 +6,7 @@ Description: Even more admin color schemes
 Version: 1.0
 Author: WordPress Core Team
 Author URI: http://wordpress.org/
-Text Domain: admin_schemes
+Text Domain: admin-color-schemes
 Domain Path: /languages
 */
 
@@ -44,9 +44,14 @@ class ACS_Color_Schemes {
 	);
 
 	function __construct() {
+		add_action( 'plugins_loaded', array( $this, 'load_plugin_textdomain' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'load_default_css' ) );
 		add_action( 'customize_controls_enqueue_scripts', array( $this, 'load_default_css' ), 1 );
 		add_action( 'admin_init' , array( $this, 'add_colors' ) );
+	}
+
+	function load_plugin_textdomain() {
+		load_plugin_textdomain( 'admin-color-schemes', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 	}
 
 	/**
@@ -56,57 +61,57 @@ class ACS_Color_Schemes {
 		$suffix = is_rtl() ? '-rtl' : '';
 
 		wp_admin_css_color( 
-			'vinyard', __( 'Vinyard', 'admin_schemes' ), 
-			plugins_url( "vineyard/colors$suffix.css", __FILE__ ),
+			'vinyard', __( 'Vinyard', 'admin-color-schemes' ), 
+			plugins_url( "colors/vineyard/colors$suffix.css", __FILE__ ),
 			array( '#301D25', '#462b36', '#d3995d', '#eabe3f' ), 
 			array( 'base' => '#f1f2f3', 'focus' => '#fff', 'current' => '#fff' )
 		);
 
 		wp_admin_css_color( 
-			'primary', __( 'Primary', 'admin_schemes' ), 
-			plugins_url( "primary/colors$suffix.css", __FILE__ ),
+			'primary', __( 'Primary', 'admin-color-schemes' ), 
+			plugins_url( "colors/primary/colors$suffix.css", __FILE__ ),
 			array( '#282b48', '#35395c', '#f38135', '#e7c03a' ),
 			array( 'base' => '#f1f2f3', 'focus' => '#fff', 'current' => '#fff' )
 		);
 
 		wp_admin_css_color( 
-			'80s-kid', __( '80\'s Kid', 'admin_schemes' ), 
-			plugins_url( "80s-kid/colors$suffix.css", __FILE__ ),
+			'80s-kid', __( '80\'s Kid', 'admin-color-schemes' ), 
+			plugins_url( "colors/80s-kid/colors$suffix.css", __FILE__ ),
 			array( '#0A3D80', '#0c4da1', '#ed5793', '#eb853b' ),
 			array( 'base' => '#e4e5e7', 'focus' => '#fff', 'current' => '#fff' )
 		);
 
 		wp_admin_css_color( 
-			'aubergine', __( 'Aubergine', 'admin_schemes' ), 
-			plugins_url( "aubergine/colors$suffix.css", __FILE__ ),
+			'aubergine', __( 'Aubergine', 'admin-color-schemes' ), 
+			plugins_url( "colors/aubergine/colors$suffix.css", __FILE__ ),
 			array( '#4c4b5f', '#585a61', '#e87162', '#da9b49' ),
 			array( 'base' => '#e4e4e7', 'focus' => '#fff', 'current' => '#fff' )
 		);
 
 		wp_admin_css_color( 
-			'cruise', __( 'Cruise', 'admin_schemes' ), 
-			plugins_url( "cruise/colors$suffix.css", __FILE__ ),
+			'cruise', __( 'Cruise', 'admin-color-schemes' ), 
+			plugins_url( "colors/cruise/colors$suffix.css", __FILE__ ),
 			array( '#292B46', '#36395c', '#8bbc9f', '#d2ac1f' ),
 			array( 'base' => '#f1f1f3', 'focus' => '#fff', 'current' => '#fff' )
 		);
 
 		wp_admin_css_color( 
-			'flat', __( 'Flat', 'admin_schemes' ), 
-			plugins_url( "flat/colors$suffix.css", __FILE__ ),
+			'flat', __( 'Flat', 'admin-color-schemes' ), 
+			plugins_url( "colors/flat/colors$suffix.css", __FILE__ ),
 			array( '#1F2C39', '#2c3e50', '#1abc9c', '#f39c12' ),
 			array( 'base' => '#f1f2f3', 'focus' => '#fff', 'current' => '#fff' )
 		);
 
 		wp_admin_css_color( 
-			'lawn', __( 'Lawn', 'admin_schemes' ), 
-			plugins_url( "lawn/colors$suffix.css", __FILE__ ),
+			'lawn', __( 'Lawn', 'admin-color-schemes' ), 
+			plugins_url( "colors/lawn/colors$suffix.css", __FILE__ ),
 			array( '#0F1515', '#1e2a29', '#5D824B', '#a7b145' ),
 			array( 'base' => '#f1f3f3', 'focus' => '#fff', 'current' => '#fff' )
 		);
 
 		wp_admin_css_color( 
-			'seashore', __( 'Seashore', 'admin_schemes' ), 
-			plugins_url( "seashore/colors$suffix.css", __FILE__ ),
+			'seashore', __( 'Seashore', 'admin-color-schemes' ), 
+			plugins_url( "colors/seashore/colors$suffix.css", __FILE__ ),
 			array( '#F8F6F1', '#d5cdad', '#7D6B5C', '#456a7f' ),
 			array( 'base' => '#533C2F', 'focus' => '#F8F6F1', 'current' => '#F8F6F1' )
 		);
