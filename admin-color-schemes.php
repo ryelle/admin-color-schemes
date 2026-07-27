527795dc36c303c0987f845df53fa6bb61eb0098
Kelly Dwan <ryelle@users.noreply.github.com>
2026-07-27 18:18:49 -0400

    Styles: Tweak colors to match generated color ramps in core

--- admin-color-schemes.php
+++ admin-color-schemes.php
@@ -50,35 +50,10 @@ function get_color_url( $color ) {
  */
 function add_colors() {
 	wp_admin_css_color(
-		// The color name needs to stay misspelled for back-compat.
-		'vinyard',
-		__( 'Vineyard', 'admin_schemes' ),
-		get_color_url( 'vineyard' ),
-		array( '#301d25', '#462b36', '#ba8752', '#eabe3f' ),
-		array(
-			'base' => '#f1f2f3',
-			'focus' => '#fff',
-			'current' => '#fff',
-		)
-	);
-
-	wp_admin_css_color(
-		'primary',
-		__( 'Primary', 'admin_schemes' ),
-		get_color_url( 'primary' ),
-		array( '#282b48', '#35395c', '#e46713', '#e7c03a' ),
-		array(
-			'base' => '#f1f2f3',
-			'focus' => '#fff',
-			'current' => '#fff',
-		)
-	);
-
-	wp_admin_css_color(
 		'80s-kid',
-		__( '80\'s Kid', 'admin_schemes' ),
+		__( '80\'s Kid', 'admin-color-schemes' ),
 		get_color_url( '80s-kid' ),
-		array( '#0c4da1', '#d13674', '#28b811' ),
+		array( '#1b4a8c', '#d13674', '#ff9abc', '#28b811' ),
 		array(
 			'base' => '#e4e5e7',
 			'focus' => '#fff',
@@ -87,10 +62,22 @@ function add_colors() {
 	);
 
 	wp_admin_css_color(
+		'adderley',
+		__( 'Adderley', 'admin-color-schemes' ),
+		get_color_url( 'adderley' ),
+		array( '#154d9b', '#2d60ab', '#bde7f0' ),
+		array(
+			'base' => '#f1f3f3',
+			'focus' => '#fff',
+			'current' => '#fff',
+		)
+	);
+
+	wp_admin_css_color(
 		'aubergine',
-		__( 'Aubergine', 'admin_schemes' ),
+		__( 'Aubergine', 'admin-color-schemes' ),
 		get_color_url( 'aubergine' ),
-		array( '#4c4b5f', '#585a61', '#ba5b32', '#da9b49' ),
+		array( '#4a437c', '#574d97', '#ba5b32', '#eab444' ),
 		array(
 			'base' => '#e4e4e7',
 			'focus' => '#fff',
@@ -99,10 +86,22 @@ function add_colors() {
 	);
 
 	wp_admin_css_color(
+		'contrast-blue',
+		__( 'High Contrast Blue', 'admin-color-schemes' ),
+		get_color_url( 'contrast-blue' ),
+		array( '#001e41', '#96c1dd', '#b2dcf6', '#9d2f4d' ),
+		array(
+			'base' => '#151923',
+			'focus' => '#151923',
+			'current' => '#151923',
+		)
+	);
+
+	wp_admin_css_color(
 		'cruise',
-		__( 'Cruise', 'admin_schemes' ),
+		__( 'Cruise', 'admin-color-schemes' ),
 		get_color_url( 'cruise' ),
-		array( '#292b46', '#36395c', '#cda200', '#79b591' ),
+		array( '#303262', '#494d88', '#2b7a52', '#cda200' ),
 		array(
 			'base' => '#f1f1f3',
 			'focus' => '#fff',
@@ -112,9 +111,9 @@ function add_colors() {
 
 	wp_admin_css_color(
 		'flat',
-		__( 'Flat', 'admin_schemes' ),
+		__( 'Flat', 'admin-color-schemes' ),
 		get_color_url( 'flat' ),
-		array( '#1f2c39', '#2c3e50', '#1abc9c', '#f39c12' ),
+		array( '#2c3e50', '#11919e', '#61c6d3', '#c04200' ),
 		array(
 			'base' => '#f1f2f3',
 			'focus' => '#fff',
@@ -123,34 +122,10 @@ function add_colors() {
 	);
 
 	wp_admin_css_color(
-		'lawn',
-		__( 'Lawn', 'admin_schemes' ),
-		get_color_url( 'lawn' ),
-		array( '#0f1515', '#1e2a29', '#636a00', '#a7b145' ),
-		array(
-			'base' => '#f1f3f3',
-			'focus' => '#fff',
-			'current' => '#fff',
-		)
-	);
-
-	wp_admin_css_color(
-		'seashore',
-		__( 'Seashore', 'admin_schemes' ),
-		get_color_url( 'seashore' ),
-		array( '#f8f6f1', '#d5cdad', '#7d6b5c', '#456a7f' ),
-		array(
-			'base' => '#533C2F',
-			'focus' => '#F8F6F1',
-			'current' => '#F8F6F1',
-		)
-	);
-
-	wp_admin_css_color(
 		'kirk',
-		__( 'Kirk', 'admin_schemes' ),
+		__( 'Kirk', 'admin-color-schemes' ),
 		get_color_url( 'kirk' ),
-		array( '#bd3854', '#5f1b29', '#321017' ),
+		array( '#4f1f27', '#8b2238', '#ad4253' ),
 		array(
 			'base' => '#fefcf7',
 			'focus' => '#fff',
@@ -159,34 +134,10 @@ function add_colors() {
 	);
 
 	wp_admin_css_color(
-		'contrast-blue',
-		__( 'High Contrast Blue', 'admin_schemes' ),
-		get_color_url( 'contrast-blue' ),
-		array( '#22466d', '#5c98c8', '#a5cde8', '#dae9f3', '#9d2f4d' ),
-		array(
-			'base' => '#151923',
-			'focus' => '#151923',
-			'current' => '#151923',
-		)
-	);
-
-	wp_admin_css_color(
-		'adderley',
-		__( 'Adderley', 'admin-color-schemes' ),
-		get_color_url( 'adderley' ),
-		array( '#bde7f0', '#216bce', '#1730e5' ),
-		array(
-			'base' => '#f1f3f3',
-			'focus' => '#fff',
-			'current' => '#fff',
-		)
-	);
-
-	wp_admin_css_color(
-		'modern-evergreen',
-		__( 'Modern Evergreen', 'admin-color-schemes' ),
-		get_color_url( 'modern-evergreen' ),
-		array( '#1e8060', '#0f4232', '#1e1e1e', '#3855e1' ),
+		'lawn',
+		__( 'Lawn', 'admin-color-schemes' ),
+		get_color_url( 'lawn' ),
+		array( '#0f1515', '#1e2a29', '#636a00', '#a7b145' ),
 		array(
 			'base' => '#f1f3f3',
 			'focus' => '#fff',
@@ -198,9 +149,58 @@ function add_colors() {
 		'modern-aubergine',
 		__( 'Modern Aubergine', 'admin-color-schemes' ),
 		get_color_url( 'modern-aubergine' ),
-		array( '#6e5ec9', '#412991', '#1e1e1e' ),
+		array( '#1d1d24', '#342a65', '#6e5ec9' ),
 		array(
 			'base' => '#f1f3f3',
+			'focus' => '#fff',
+			'current' => '#fff',
+		)
+	);
+
+	wp_admin_css_color(
+		'modern-evergreen',
+		__( 'Modern Evergreen', 'admin-color-schemes' ),
+		get_color_url( 'modern-evergreen' ),
+		array( '#0f231a', '#265843', '#1e8060' ),
+		array(
+			'base' => '#f1f3f3',
+			'focus' => '#fff',
+			'current' => '#fff',
+		)
+	);
+
+	wp_admin_css_color(
+		'primary',
+		__( 'Primary', 'admin-color-schemes' ),
+		get_color_url( 'primary' ),
+		array( '#282b48', '#3c4065', '#bf4500', '#e4b903' ),
+		array(
+			'base' => '#f1f2f3',
+			'focus' => '#fff',
+			'current' => '#fff',
+		)
+	);
+
+	wp_admin_css_color(
+		'seashore',
+		__( 'Seashore', 'admin-color-schemes' ),
+		get_color_url( 'seashore' ),
+		array( '#eee5d6', '#d9c8aa', '#456a7f' ),
+		array(
+			'base' => '#533C2F',
+			'focus' => '#F8F6F1',
+			'current' => '#F8F6F1',
+		)
+	);
+
+	wp_admin_css_color(
+		// The color name needs to stay misspelled for back-compat.
+		'vinyard',
+		__( 'Vineyard', 'admin-color-schemes' ),
+		get_color_url( 'vineyard' ),
+		array( '#432531', '#934f69', '#ab7844', '#ffe9c6' ),
+		array(
+			'base' => '#f1f2f3',
 			'focus' => '#fff',
 			'current' => '#fff',
 		)
